9fccf4b24570c032093abb0ef2f23a285065c2b0
ParkhomenkoDV <parkho.m.enko@mail.ru>
2024-10-27 01:07:49 +0300

    2024-10-27-01-07

--- spline/tests/conftest.py
+++ spline/tests/conftest.py
@@ -1,22 +1,42 @@
 from numpy import random
 import pytest
 
-from spline import Spline
+from spline import STANDARDS, Spline
 
+
+def standard() -> str:
+    return random.choice(('1139', '6033', '100092'))
+
+
+def join(standard) -> str:
+    if standard == '1139':
+        return random.choice(('inner', 'outer', 'left', 'right'))
+    elif standard == '6033':
+        return random.choice(('outer', 'left', 'right'))
+    elif standard == '100092':
+        return random.choice(('left', 'right'))
+
+def parameters(standard) -> dict:
+    row = STANDARDS[standard]['standard'].sample(n=1)
+    if standard == '1139':
+        return {'n_teeth': int(row['n_teeth'].iloc[0]), 'd': float(row['d'].iloc[0]), 'D': float(row['D'].iloc[0])}
+    elif standard == '6033':
+        row = row.loc[:, (row != 0).all(axis=0)]
+        module = random.choice(row.columns)
+        return {'n_teeth': int(row[module].iloc[0]), 'module': float(module), 'D': float(row.index[0])}
+    elif standard == '100092':
+        return {'n_teeth': int(row['n_teeth'].iloc[0]), 'module': float(row['module'].iloc[0]), 'd': float(row['d'].iloc[0])}
 
 @pytest.fixture(scope='function')
-def standard() -> int:
-    return random.choice((1139, 6033, 100092))
+def spline():
+    s = standard()
+    return Spline(s, join(s), **parameters(s))
 
+def moment():
+    return random.uniform(-800, 800)
 
-@pytest.fixture(scope='function')
-def join() -> str:
-    return random.choice(('inner', 'outer', 'left', 'right'))
+def length():
+    return random.uniform(3, 50) / 1_000
 
-
-@pytest.fixture(scope='function')
-def spline(standard):
-    if standard == 1139:
-        return Spline(standard, random.choice(('inner', 'outer', 'left', 'right')))
-    elif standard == 6033:
-        return Spline(standard, random.choice(('outer', 'left', 'right')))
+def max_tension():
+    return random.uniform(3, 50) * 10 ** 6
--- spline/tests/test_spline.py
+++ spline/tests/test_spline.py
@@ -1,19 +1,53 @@
 from numpy import random
 import pytest
 
-from spline import Spline
+from conftest import standard, join, parameters, moment, length, max_tension
+
+from spline import STANDARDS, Spline
 
 
-def test_init_spline(standard):
-    if standard == 1139:
-        assert Spline(standard, random.choice(('inner', 'outer', 'left', 'right')))
-    elif standard == 6033:
-        assert Spline(standard, random.choice(('outer', 'left', 'right')))
+def test_init_spline1139():
+    s = '1139'
+    for j in ('left', 'right', 'inner', 'outer'):
+        for _, row in STANDARDS[s]['standard'].iterrows():
+            assert Spline(s, j, n_teeth=row['n_teeth'], d=row['d'], D=row['D'])
+
+def test_init_spline6033():
+    s = '6033'
+    for j in ('left', 'right', 'outer'):
+        for D, row in STANDARDS[s]['standard'].iterrows():
+            row=row[row > 0].to_dict()
+            for module, n_teeth in row.items():
+                assert Spline(s, j, n_teeth=n_teeth, module=module, D=D)
+
+def test_init_spline100092():
+    s = '100092'
+    for j in ('left', 'right'):
+        for _, row in STANDARDS[s]['standard'].iterrows():
+            assert Spline(s, j, n_teeth=row['n_teeth'], module=row['module'], d=row['d'])
+
+def test_init_spline():
+    for _ in range(1_000):
+        s = standard()
+        assert Spline(s, join(s), **parameters(s))
 
 
 def test_spline_tension():
-    pass
+    for _ in range(1_000):
+        s = standard()
+        sp = Spline(s, join(s), **parameters(s))
+        assert sp.tension(moment(), length())
 
 
 def test_fit_spline():
-    pass
+    for s in STANDARDS.keys():
+        if s == '1139':
+            joins = ('left', 'right', 'inner', 'outer')
+        elif s == '6033':
+            joins = ('left', 'right', 'outer')
+        elif s == '100092': 
+            joins = ('left', 'right')
+        for j in joins:
+            assert len(Spline.fit(s, j, max_tension(), moment(), length())) >= 0
+
+
